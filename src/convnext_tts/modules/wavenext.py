311dae75db2624d7bbe7986136ce6a7f8676d73e
reppy4620 <ilikeniku@icloud.com>
2024-04-26 12:31:40 +0900

    fix wavenext and update readme

--- src/convnext_tts/modules/wavenext.py
+++ src/convnext_tts/modules/wavenext.py
@@ -3,8 +3,18 @@ from convnext_tts.layers.convnext import ConvNeXtLayer
 
 
 class WaveNeXt(nn.Module):
-    def __init__(self, in_channels, channels, h_channels, hop_length, num_layers):
+    def __init__(
+        self,
+        in_channels,
+        channels,
+        h_channels,
+        n_fft,
+        hop_length,
+        num_layers,
+        apply_tanh=True,
+    ):
         super().__init__()
+        self.apply_tanh = apply_tanh
         self.in_conv = nn.Conv1d(in_channels, channels, kernel_size=7, padding=3)
         self.norm = nn.LayerNorm(channels)
         scale = 1 / num_layers
@@ -12,7 +22,8 @@ class WaveNeXt(nn.Module):
             [ConvNeXtLayer(channels, h_channels, scale) for _ in range(num_layers)]
         )
         self.norm_last = nn.LayerNorm(channels)
-        self.out_conv = nn.Conv1d(channels, hop_length, 1)
+        self.out_conv = nn.Conv1d(channels, n_fft, 1)
+        self.fc = nn.Conv1d(n_fft, hop_length, 1)
 
     def forward(self, x):
         x = self.in_conv(x)
@@ -21,6 +32,8 @@ class WaveNeXt(nn.Module):
             x = layer(x)
         x = self.norm_last(x.transpose(1, 2)).transpose(1, 2)
         o = self.out_conv(x)
+        o = self.fc(x)
         o = o.reshape(o.size(0), 1, -1)
-        o = o.tanh()
+        if self.apply_tanh:
+            o = o.tanh()
         return o
